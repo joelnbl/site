b88203a4658e2309008ea761080bb3665a252eb5
Joel Bello <joel8bl@gmail.com>
2024-12-28 14:59:48 +0100

    adding first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,43 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 bg-white text-gray-900">
+          <main
+            className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-[60ch] mx-auto w-full space-y-6`}
+          >
+            {children}
+          </main>
+          <Footer />
+        </div>
       </body>
     </html>
+  );
+}
+
+function Footer() {
+  const links = [
+    { name: '@', url: 'https://x.com/leeerob' },
+    { name: 'youtube', url: 'https://www.youtube.com/@' },
+    { name: 'linkedin', url: 'https://www.linkedin.com/in/leeerob' },
+    { name: 'github', url: 'https://github.com/' },
+  ];
+
+  return (
+    <footer className="mt-12 text-center">
+      <div className="flex justify-center space-x-4 tracking-tight">
+        {links.map((link) => (
+          <a
+            key={link.name}
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-400 hover:text-blue-500 transition-colors duration-200"
+          >
+            {link.name}
+          </a>
+        ))}
+      </div>
+    </footer>
   );
 }
